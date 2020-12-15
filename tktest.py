094f517358225eae4cf616afcff227502844b383
Ginga Ishiguro <ginga@KPUnoiMac.local>
2020-12-15 16:24:00 +0900

    add: ボタンの表示と切り替え、関数

--- tktest.py
+++ tktest.py
@@ -3,10 +3,27 @@ import tkinter as tk
 
 root = tk.Tk()
 
+def pushed(b):
+    b["text"] = "開始します"
+
 #ウィンドウタイトル
 root.title("Tkinter Window")
 
 #ウィンドウサイズ
 root.geometry("1280x720")
 
+
+#ラベルを追加
+label = tk.Label(root, text="Hello,World")
+
+#表示
+label.grid()
+
+button =tk.Button(root, text="訓練開始", command= lambda : pushed(button))
+button.grid()
+
+    
+
+
+#ウィンドウの表示
 root.mainloop()
